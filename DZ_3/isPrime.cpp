4972ccbc7c5935ad298dc7f3e3734885b240a806
MaxKostin03 <94163240+MaxKostin03@users.noreply.github.com>
2022-06-07 14:05:24 +0300

    Add files via upload

---
+++ DZ_3/isPrime.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+
+void _fastcall isPrime(int number)
+{
+    int i = 1;
+    int k = 0;
+    while (i <= number)
+    {
+        if (number % i++ == 0)
+        {
+            k++;
+        }
+        else
+        {
+            continue;
+        }
+        if (k == 3)
+        {
+            break;
+        }
+    }
+    if (k == 2)
+    {
+        cout << "Введенное число " << number << " является простым" << endl;
+    }
+    else
+        cout << "Введенное число " << number << " не является простым" << endl;
+}
+
+
+
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");   
+    int c;
+    cout << "Введите число: ";
+    cin >> c;
+    
+    isPrime(c);
+    
+}
